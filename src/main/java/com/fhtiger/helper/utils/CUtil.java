ab5b2659d999a5bdf777fab129ecf2148e1aa9e6
Chivenh <guishizuiezhishi@163.com>
2023-05-25 10:24:22 +0800

    feature(CUtil) add CUtil.

---
+++ src/main/java/com/fhtiger/helper/utils/CUtil.java
@@ -0,0 +1,46 @@
+package com.fhtiger.helper.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * CUtil
+ * {@link java.util.Collection}容器的便捷创建工厂类
+ *
+ * @author LFH
+ * @created 2023年05月19日 16:55
+ */
+@SuppressWarnings({ "unused", "AlibabaClassNamingShouldBeCamel" })
+
+public final class CUtil {
+	private CUtil() throws IllegalAccessException {
+		throw new IllegalAccessException("此类不能被实例化!");
+	}
+
+	/**
+	 * 自动计算合理(initialCapacity)来初始化Map容器,默认使用{@link HashMap#HashMap(int)}
+	 *
+	 * @param size 初始化容器大小,自动计算合理的Map:initialCapacity
+	 * @param <K>  Key
+	 * @param <V>  Value
+	 * @return Map
+	 */
+	public static <K, V> Map<K, V> newMap(int size) {
+		return newMap(size, HashMap::new);
+	}
+
+	/**
+	 * 自动计算合理(initialCapacity)来初始化Map容器
+	 *
+	 * @param size    初始化容器大小,自动计算合理的Map:initialCapacity
+	 * @param creator 容器创建器{@link HashMap#HashMap(int)},{@link java.util.LinkedHashMap#LinkedHashMap(int)}等.
+	 * @param <K>     Key
+	 * @param <V>     Value
+	 * @return Map
+	 */
+	public static <K, V> Map<K, V> newMap(int size, Function<Integer, Map<K, V>> creator) {
+		int initialCapacity = size < 1 ? 0 : size / 3 * 4;
+		return creator.apply(initialCapacity);
+	}
+}
