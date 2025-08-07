3b88963a600e5ba796ebb531943696ae5be1bd5f
Chivenh <guishizuiezhishi@163.com>
2025-08-07 14:32:32 +0800

    OutName perfect..........

--- src/main/java/com/fhtiger/helper/utils/web/OutputNameGenerator.java
+++ src/main/java/com/fhtiger/helper/utils/web/OutputNameGenerator.java
@@ -19,38 +19,39 @@ public enum OutputNameGenerator {
 	/**
 	 * 浏览器环境下自动根据不同浏览器进行命名转换
 	 */
-	BROWSER((arg)->{
+	BROWSER(OutputNameGenerator::browserFileNameExtracted),
+	/**
+	 * 自然模式，只trim
+	 */
+	NATIVE((arg)-> String::trim);
 
+	private static UnaryOperator<String> browserFileNameExtracted(Object arg) {
 		assert arg instanceof HttpServletRequest : "浏览器命名生成器需要(HttpServletRequest)参数";
 
-		HttpServletRequest request = (HttpServletRequest)arg;
+		HttpServletRequest request = (HttpServletRequest) arg;
 
 		final String userAgent = request.getHeader("USER-AGENT");
 
-		return (name)->{
+		return (name) -> {
 			String finalFileName = name;
-			final String browserTagiE="MSIE",browserTagMoz="Mozilla";
+			final String browserTagiE = "MSIE", browserTagMoz = "Mozilla";
 
 			try {
-				if(userAgent.contains(browserTagiE)){
+				if (userAgent.contains(browserTagiE)) {
 					/*IE浏览器*/
-					finalFileName = URLEncoder.encode(name,StandardCharsets.UTF_8.displayName());
-				}else if(userAgent.contains(browserTagMoz)){
+					finalFileName = URLEncoder.encode(name, StandardCharsets.UTF_8.displayName());
+				} else if (userAgent.contains(browserTagMoz)) {
 					/*google,火狐浏览器*/
 					finalFileName = new String(name.getBytes("GBK"), StandardCharsets.ISO_8859_1);
-				}else{
-					finalFileName = URLEncoder.encode(name,StandardCharsets.UTF_8.displayName());//其他浏览器
+				} else {
+					finalFileName = URLEncoder.encode(name, StandardCharsets.UTF_8.displayName());//其他浏览器
 				}
 			} catch (UnsupportedEncodingException e) {
 				/**/
 			}
 			return finalFileName;
 		};
-	}),
-	/**
-	 * 自然模式，只trim
-	 */
-	NATIVE((arg)-> String::trim);
+	}
 
 	public static final UnaryOperator<String> NATIVE_NAME_GENERATOR=NATIVE.getGenerator(null);
 
